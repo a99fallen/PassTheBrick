5353ab8209b1fe3c829a3f199418bdcd91351ac8
Grzegorz Goleniowski <grzegorz.goleniowski@nokia.com>
2023-10-25 08:47:26 +0200

    build :: minor name change of main class

--- src/main/java/com/PassTheBrick/PassTheBrickApplication.java
+++ src/main/java/com/PassTheBrick/PassTheBrickApplication.java
@@ -1,13 +1,13 @@
-package com.passthebrick;
+package com.PassTheBrick;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PassthebrickApplication {
+public class PassTheBrickApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PassthebrickApplication.class, args);
+		SpringApplication.run(PassTheBrickApplication.class, args);
 	}
 
 }
--- src/test/java/com/PassTheBrick/PassthebrickApplicationTests.java
+++ src/test/java/com/PassTheBrick/PassthebrickApplicationTests.java
@@ -1,4 +1,4 @@
-package com.passthebrick;
+package com.PassTheBrick;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
